bd07de21bc63d7fced8989b0d471d2f6c10e58da
shivrya <shivrajkhose4@gmail.com>
2025-07-04 16:34:02 -0700

    Day 1 of Python

---
+++ Day1-Intro-to-Python/first.py
@@ -0,0 +1,13 @@
+'''
+Python is an Interpreted language
+Open-source programming language
+Rich library support like numPy, Pandas, Tensorflow
+'''
+
+'''
+most of the Python usage today is Python 3 because : 
+1. Python 2 reached end-of-life on January 1, 2020
+2. All major libraries (e.g., pandas, tensorflow, scikit-learn) have dropped support for Python 2.
+3. Modern systems and environments (like Linux distros, macOS, Windows, Anaconda) all come with Python 3 by default.
+'''
+print("hello world")
---
+++ Day3-Module-and-Pip/module.py
@@ -0,0 +1,12 @@
+
+
+# External modules : you need to install these modules using "pip" like pandas, numpy, matplotlib, flask
+# example : pip3 install pandas
+
+# In-built modules : these modules are built-in python language, no need to install like os, math, sys, datetime
+
+
+# pip : default package installer for python 2.x 
+# pip3 : this explicitely refers to pip for python 3. Most common gis
+
+
