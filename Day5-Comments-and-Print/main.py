319798f538500737db2aa43313a23e5767f70bfb
shivrya <shivrajkhose4@gmail.com>
2025-07-04 17:01:48 -0700

    Day 5: Comments and Print statements
    exit
    :wq
    
    exit
    
    exit();

---
+++ Day5-Comments-and-Print/main.py
@@ -0,0 +1,26 @@
+
+# Single line comment : this line wont be executed by the interpreter 
+
+'''
+this is a multi line comment
+jhfadksj
+jsfklsd
+fjdkljs
+'''
+
+"""
+double quotes also works for a multi line comment
+jfhds
+fjsdkjf
+"""
+
+
+
+print("Hello world\nthis is my first 2 lines print statement")
+
+print("Hello world")     # printing hello world
+
+
+print("my name is \"Shivraj\"")  # my name is "Shivraj"
+
+print("hello",6,7,sep="*", end="009")   # hello*6*7009
