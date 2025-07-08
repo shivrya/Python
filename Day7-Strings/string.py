b5bf0273ebdfe917f88ad9ac2ed693ae44f33baa
shivrya <shivrajkhose4@gmail.com>
2025-07-07 19:18:12 -0700

    Python Strings

---
+++ Day7-Strings/string.py
@@ -0,0 +1,24 @@
+
+# String is a sequence of characters
+
+
+# string in single quotes
+a = 'my name is shivraj'
+
+# string in double quotes
+b = "my name is shivraj khose"
+
+# mutli line string using triple quotes
+c = ''' 
+        my 
+        name
+        is
+        shivraj
+    '''
+
+
+print(c)
+
+
+# string is immutable in java
+ 
